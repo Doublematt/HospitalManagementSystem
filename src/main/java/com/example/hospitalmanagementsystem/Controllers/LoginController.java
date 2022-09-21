cc25b061e1b5f1e3a7e5a3c14e10acf484017f0a
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-09-21 11:40:44 +0200

    Create account form, exit button Created create account form and two functions changePane (login, create account) and exit ( exitButton + alt F4)

--- src/main/java/com/example/hospitalmanagementsystem/Controllers/LoginController.java
+++ src/main/java/com/example/hospitalmanagementsystem/Controllers/LoginController.java
@@ -1,8 +1,93 @@
 package com.example.hospitalmanagementsystem.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class LoginController {
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private Hyperlink LinkedinLink;
+
+    @FXML
+    private Hyperlink createAccountLink;
+
+    @FXML
+    private AnchorPane createAccountPane;
+
+    @FXML
+    private Hyperlink githubLink;
+
+    @FXML
+    private Button logSubmitButton;
+
+    @FXML
+    private Hyperlink loginLink;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private PasswordField regConfirmPasswordField;
+
+    @FXML
+    private TextField regEmailField;
+
+    @FXML
+    private TextField regFirstNameField;
+
+    @FXML
+    private TextField regLastNameField;
+
+    @FXML
+    private TextField regLoginField;
+
+    @FXML
+    private PasswordField regPasswordField;
+
+    //no FXML variable
+    private Alert alert;
+    private Stage stage;
+
+    /*
+    Function to change between login and register
+    Connected to login hyperlink and create account hyperlink
+     */
+
+    public void changePane (ActionEvent e){
+        if(e.getSource() == loginLink){
+            loginPane.setVisible(true);
+            createAccountPane.setVisible(false);
+        }else if(e.getSource() == createAccountLink){
+            loginPane.setVisible(false);
+            createAccountPane.setVisible(true);
+        }
+    }
+
+    /*
+    Function connected to exit button
+    Exits the app
+     */
+    public void exit(ActionEvent event){
+
+        //alert instance
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("");
+        alert.setContentText("Are you sure? ");
+
+        if (alert.showAndWait().get() == ButtonType.OK){
+            stage = (Stage) mainPane.getScene().getWindow();
+            System.out.println("exit now");
+            stage.close();
+
+        }
+
+    }
 
 }
--- src/main/java/com/example/hospitalmanagementsystem/Main.java
+++ src/main/java/com/example/hospitalmanagementsystem/Main.java
@@ -3,7 +3,10 @@ package com.example.hospitalmanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,8 +16,33 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hospital Management system");
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
+
+        //implementing the below exit function
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            exit(stage);
+        });
+    }
+
+    /*
+        Function to exit the application when alt + F4 is pressed
+        Creates alert to make sure the user wants to close app
+        Exits the app
+     */
+    public void exit(Stage stage){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("");
+        alert.setContentText("Are you sure? ");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+            System.out.println("close now");
+            stage.close();
+        }
+
     }
 
     public static void main(String[] args) {

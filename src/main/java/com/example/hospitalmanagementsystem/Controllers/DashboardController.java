43829f49b9df8b3df4592196e21cedebd370e71c
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-09-29 22:40:25 +0200

    images

--- src/main/java/com/example/hospitalmanagementsystem/Controllers/DashboardController.java
+++ src/main/java/com/example/hospitalmanagementsystem/Controllers/DashboardController.java
@@ -1,4 +1,28 @@
 package com.example.hospitalmanagementsystem.Controllers;
 
-public class DashboardController {
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController  implements Initializable {
+
+
+
+    @FXML
+    private AnchorPane menuPane;
+
+    @FXML
+    private ImageView newsImage1, newsImage2, newsImage3;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
 }

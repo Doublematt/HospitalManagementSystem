eef048202c9a730d2d3c566fdbd31ed6036fd952
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-09-30 08:12:58 +0200

    dashboard design

--- src/main/java/com/example/hospitalmanagementsystem/Controllers/DashboardController.java
+++ src/main/java/com/example/hospitalmanagementsystem/Controllers/DashboardController.java
@@ -1,20 +1,26 @@
 package com.example.hospitalmanagementsystem.Controllers;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static java.lang.Thread.sleep;
 
 public class DashboardController  implements Initializable {
 
 
 
     @FXML
-    private AnchorPane menuPane;
+    private AnchorPane menuPane, newsPane;
 
     @FXML
     private ImageView newsImage1, newsImage2, newsImage3;
@@ -22,7 +28,11 @@ public class DashboardController  implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+
+
+
     }
+
 
 
 }
